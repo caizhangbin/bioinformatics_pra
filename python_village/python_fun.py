2242e78812931ff862da328893749cb5ea898d1c
caizhangbin <caizhangbin@gmail.com>
2024-05-31 22:20:47 -0400

    update

--- python_village/python_fun.py
+++ python_village/python_fun.py
@@ -76,11 +76,19 @@ f2.close
 # Return: The number of occurrences of each word in s, where words are separated by spaces. Words are case-sensitive, and the lines in the output can be in any order.
 ###
 
-str = "We tried list and we tried dicts also we tried Zen"
+str = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
 
 for word in str.split(' '):
     print (word)
 
+i = 0
+dict = {}
+for word in str.split(' '):
+    if word in dict:
+        dict[word] += 1
+    else:
+        dict[word] = 1
+
 for key, value in dict.items():
-    print (key)
-    print (value)
+    print (key, value)
+    
