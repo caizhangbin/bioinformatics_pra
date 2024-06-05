ef26a44c8dfaae77ddf42262e28d6866f8dc381a
caizhangbin <caizhangbin@gmail.com>
2024-06-05 13:58:27 -0400

    update

--- Python-Workshop/main.py
+++ Python-Workshop/main.py
@@ -1,2 +1,30 @@
-print('hey!')
-my_name = 'Jack'
+import pandas as pd
+
+df = pd.read_csv("../Python-Workshop/data.csv")
+df['class'].unique()
+classes = df['class'].unique()
+
+for iris in classes:
+  class_df = df.loc[ df['class']==iris, : ]
+  print(iris)
+  print(class_df.describe())
+
+
+import matplotlib.pyplot as plt
+
+data = [0,1,2,3,4,7,10,16,15,8,4,7,3,1,0]
+
+plt.plot(data)
+plt.show()
+
+
+classes = df['class'].unique()
+
+for iris in classes:
+  iris_df = df.loc[df['class']==iris, :]
+  x = iris_df['petal_length']
+  y = iris_df['petal_width']
+  plt.scatter(x, y)
+
+plt.legend(classes)
+plt.show()
